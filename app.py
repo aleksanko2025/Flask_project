537aea249078f601269310f0e07a1ebd8fea14d0
Aleksandr Kosenko <aleksanko14@gmail.com>
2025-05-14 01:28:42 +0200

    Terminados listas y detalles, falta insertar fotos

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import json
 app = Flask(__name__)	
 
 
@@ -10,12 +11,41 @@ def portada():
 def inicio():
     return render_template("inicio.html") 
 
-@app.route('/resultados')
+@app.route('/resultados', methods=['POST'])
 def resultados():
-    return render_template("listas.html")
+    ubicacion_usuario = request.form['busqueda']
 
-@app.route('/detalles')
-def detalles():
-    return render_template("detalles.html")
+    # Si el campo está vacío, mostrar todos los resultados
+    if not ubicacion_usuario:
+        with open('templates/lugares_naturales.json', encoding='utf-8') as f:
+            data = json.load(f)
+            lugares = data["lugares_naturales_impresionantes"]
+        return render_template('listas.html', lugares=lugares, ubicacion="Todos los lugares")
+    
+    # Asegúrate de que la ruta al archivo JSON sea correcta
+    with open('templates/lugares_naturales.json', encoding='utf-8') as f:
+        data = json.load(f)
+        lugares = data["lugares_naturales_impresionantes"]
+
+    # Filtrar lugares por coincidencia en la ubicación
+    resultados_filtrados = [
+        lugar for lugar in lugares
+        if ubicacion_usuario in lugar["ubicación"]
+    ]
+
+    return render_template('listas.html', lugares=resultados_filtrados, ubicacion=ubicacion_usuario)
+
+
+@app.route('/detalles/<lugar_nombre>')
+def detalles(lugar_nombre):
+    with open('templates/lugares_naturales.json', encoding='utf-8') as f:
+        data = json.load(f)
+        # Buscar el lugar en la lista de lugares
+        lugar = next((lugar for lugar in data["lugares_naturales_impresionantes"] if lugar["nombre"].lower() == lugar_nombre.lower()), None)
+    
+    if lugar:
+        return render_template('detalles.html', lugar=lugar)
+    else:
+        return "Lugar no encontrado", 404
 
 app.run("0.0.0.0",5000,debug=True)
